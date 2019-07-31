8e4507e56a5a3e5e22bcf8b9035a0daec33a219b
ashish <ashishgy77@gmail.com>
2019-07-31 22:12:43 +0530

    add models

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.genric import (TemplateView, ListView)
+from .models import Post, Comment
+# creating views using class
 
-# Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_query(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
